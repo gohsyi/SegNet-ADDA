69541e804984512231688600216da6b6f99fb0c0
Henry Guo <guohongyi@sjtu.edu.cn>
2019-06-18 16:03:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ tf.app.flags.DEFINE_string('save_image', "../logs/segmentation/", """ whether to
 # experiment setting
 tf.app.flags.DEFINE_string('loss', 'dice', """ normal/weighted/dice """)
 tf.app.flags.DEFINE_string('test', '', """ checkpoint file """)
-tf.app.flags.DEFINE_string('transfer', '', """ checkpoint dir, like 'logs/dice/' """)
 tf.app.flags.DEFINE_string('finetune', '', """ finetune checkpoint file """)
 tf.app.flags.DEFINE_integer('batch_size', "5", """ batch_size """)
 tf.app.flags.DEFINE_integer('max_steps', "1000000", """ max training steps """)
@@ -29,47 +28,16 @@ tf.app.flags.DEFINE_integer('image_c', "3", """ image channel (RGB) """)
 tf.app.flags.DEFINE_integer('num_classes', "20", """ 20 kinds of objects """)
 
 
-def checkArgs():
-    if FLAGS.test != '':
-        print('The model is set to testing')
-        print("check point file: %s" % FLAGS.test)
-    elif FLAGS.transfer != '':
-        print('The model is set to transfer learning')
-        print('check point file : %s' % FLAGS.transfer)
-    elif FLAGS.finetune != '':
-        print('The model is set to Finetune from ckpt')
-        print("check point file: %s" % FLAGS.finetune)
-        print("Image dir: %s" % 'train.txt')
-    else:
-        print('The model is set to training')
-        print("Max training iteration: %d" % FLAGS.max_steps)
-        print("Initial lr: %f" % FLAGS.learning_rate)
-        print("Image dir: %s" % 'train.txt')
-
-    if FLAGS.loss != 'normal' and FLAGS.loss != 'weighted' and FLAGS.loss != 'dice':
-        print("loss function not implemented")
-        raise ValueError
-
-    print("GPU Device: %s" % FLAGS.gpu)
-    print("Batch Size: %d" % FLAGS.batch_size)
-    print("Loss Function: %s" % FLAGS.loss)
-
-
 def main(args):
-    checkArgs()
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = FLAGS.gpu
 
     if FLAGS.test:
         sn = SegNet(FLAGS)
         sn.test()
-    elif FLAGS.transfer:
-        adda = ADDA(FLAGS)
-        adda.train()
     else:
         sn = SegNet(FLAGS)
         sn.train()
-
 
 if __name__ == '__main__':
     tf.app.run()
